4e89aeaa26e81b27b1efd5e066f1b36a5d8c5d56
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 14:22:28 +0530

    Added ClassC.java

---
+++ CodingGoat/src/inheritanceexamples/ClassC.java
@@ -0,0 +1,14 @@
+package inheritanceexamples;
+
+public class ClassC {
+
+	public static void main(String[] args) {
+		
+		ClassB cb = new ClassB();
+		cb.display();
+		System.out.println(cb.text);
+		System.out.println(cb.x);
+
+	}
+
+}
