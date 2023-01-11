080187db39ffc83c82c024f8b5f0eca07088d1a6
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 14:24:10 +0530

    Added Identity.java

---
+++ CodingGoat/src/abstractionconcept/Identity.java
@@ -0,0 +1,11 @@
+package abstractionconcept;
+
+public abstract class Identity {
+
+	abstract void getName(String name);
+
+	abstract void getGender(String gender);
+
+	abstract void getCity(String city);
+
+}
