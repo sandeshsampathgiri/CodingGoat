2ef4f980fd16d61d698eb0f0dd4862e5c4fde281
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 14:25:02 +0530

    Added MySum.java

---
+++ CodingGoat/src/encapsulationconcept/MySum.java
@@ -0,0 +1,14 @@
+package encapsulationconcept;
+
+public class MySum {
+
+	public static void main(String[] args) {
+
+		Addition a = new Addition();
+		a.setX(10);
+		a.setY(20);
+		a.setZ(30);
+		a.sum();
+	}
+
+}
