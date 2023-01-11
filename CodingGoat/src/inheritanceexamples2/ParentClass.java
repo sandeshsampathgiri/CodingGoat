6c76d4993d87b765fb51e59bf7041ce5ad6e113e
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 14:25:22 +0530

    Added ParentClass.java

---
+++ CodingGoat/src/inheritanceexamples2/ParentClass.java
@@ -0,0 +1,18 @@
+package inheritanceexamples2;
+
+public class ParentClass {
+	
+	int x=10;
+	int p=20;
+	
+	public void msg()
+	{
+		System.out.println("Printing from parent class");
+	}
+	
+	public void parentTest()
+	{
+		System.out.println("Parent class specific method");
+	}
+
+}
