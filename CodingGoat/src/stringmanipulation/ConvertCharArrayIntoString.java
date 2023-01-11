3c2f82f36dbc626f16a4c677593373dc2698b654
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 17:18:09 +0530

    Added ConvertStringToCharArray.java

---
+++ CodingGoat/src/stringmanipulation/ConvertCharArrayIntoString.java
@@ -0,0 +1,15 @@
+package stringmanipulation;
+
+import java.util.Scanner;
+
+public class ConvertCharArrayIntoString {
+
+	public static void main(String[] args) {
+
+		char[] chArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'I', 'n', 'd', 'i', 'a' };
+		String str = String.valueOf(chArray);
+		System.out.println("String= " + str);
+
+	}
+
+}
