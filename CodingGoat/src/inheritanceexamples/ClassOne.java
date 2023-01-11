9ca8496ccca1d3126311566637093f6609875f9b
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 13:41:03 +0530

    Added ClassOne.java

---
+++ CodingGoat/src/inheritanceexamples/ClassOne.java
@@ -0,0 +1,17 @@
+package inheritanceexamples;
+
+public class ClassOne {
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+
+	public void m1() {
+		System.out.println("Parent Class-m1");
+	}
+
+	public void m2() {
+		System.out.println("Parent Class-m2");
+	}
+
+}
