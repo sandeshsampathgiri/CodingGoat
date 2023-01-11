8ecba1e9ac3922a0398034cc4bbbf9a1bc55e81a
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 14:26:31 +0530

    Added Student.java

---
+++ CodingGoat/src/encapsulationconcept/Student.java
@@ -0,0 +1,42 @@
+package encapsulationconcept;
+
+public class Student {
+
+	private String studentName;
+	private int studentAge;
+	private String parentName;
+	private String dateOfBirth;
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentAge() {
+		return studentAge;
+	}
+
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
