a2de08f9a932bb791abf6de3a8cf93d7f7a8df88
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-11 13:39:52 +0530

    Added AB.java

---
+++ CodingGoat/src/abstractionconcept/AB.java
@@ -0,0 +1,10 @@
+package abstractionconcept;
+
+public class AB {
+
+	public static void main(String[] args) {
+
+		A.m1(); // Calling static method of interface A.
+		B.m1(); // Calling static method of interface B.
+	}
+}
