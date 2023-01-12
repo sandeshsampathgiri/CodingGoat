6cb5dcbd6faed37e646e33ca1aa557f331569b11
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-12 13:43:17 +0530

    Added StringConcatenationTest1.java

---
+++ CodingGoat/src/stringmanipulation/StringConcatenationTest1.java
@@ -0,0 +1,10 @@
+package stringmanipulation;
+
+public class StringConcatenationTest1 {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		s1.concat("Java");
+		System.out.println(s1); // Hello
+	}
+}
