efe3c5678d6d02745978bb2fc63ac827b3772dca
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-12 17:50:30 +0530

    Added MutableStringExample.java

---
+++ CodingGoat/src/stringmanipulation/MutableStringExample.java
@@ -0,0 +1,12 @@
+package stringmanipulation;
+
+public class MutableStringExample {
+
+	public static void main(String[] args) {
+
+		StringBuffer sb = new StringBuffer("Scientech");
+		sb.append(" Easy");
+		System.out.println(sb);
+
+	}
+}
