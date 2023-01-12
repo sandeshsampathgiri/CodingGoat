9d35b8540a16398452c0d25cf961aa4b4d715fb3
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-12 13:43:51 +0530

    StringConcatenationTest4.java

---
+++ CodingGoat/src/stringmanipulation/StringConcatenationTest4.java
@@ -0,0 +1,9 @@
+package stringmanipulation;
+
+public class StringConcatenationTest4 {
+
+	public static void main(String[] args) {
+		String s1 = "Shubh" + " Deep";
+		System.out.println(s1);
+	}
+}
