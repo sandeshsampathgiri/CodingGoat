de48456e01cc70c5fc123094289aa2a4418ceab8
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-12 13:44:32 +0530

    Added ToUpperCase1.java

---
+++ CodingGoat/src/stringmanipulation/ToUpperCase1.java
@@ -0,0 +1,10 @@
+package stringmanipulation;
+
+public class ToUpperCase1 {
+
+	public static void main(String[] args) {
+		String s = new String("technology");
+		String s2 = s.toUpperCase();
+		System.out.println(s2);
+	}
+}
