3d3aeb0884effebed555c6f2d204cf8e5dea12e6
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-12 13:44:00 +0530

    StringConcatenationTest5.java

---
+++ CodingGoat/src/stringmanipulation/StringConcatenationTest5.java
@@ -0,0 +1,12 @@
+package stringmanipulation;
+
+public class StringConcatenationTest5 {
+
+	public static void main(String[] args) {
+		int date = 15;
+		String month = " August ";
+		int year = 1947;
+		String s1 = date + month + year;
+		System.out.println(s1);
+	}
+}
