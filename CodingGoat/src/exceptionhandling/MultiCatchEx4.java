f272e574d4b3d92f6fc96e15a350de3985f8915c
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:32:05 +0530

    Added MultiCatchEx4.java

---
+++ CodingGoat/src/exceptionhandling/MultiCatchEx4.java
@@ -0,0 +1,27 @@
+package exceptionhandling;
+
+import java.util.Scanner;
+
+public class MultiCatchEx4 {
+
+	public static void main(String[] args) {
+
+		int x, y;
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter your first number");
+			x = Integer.parseInt(sc.nextLine());
+			System.out.println("Enter your second number");
+			y = Integer.parseInt(sc.nextLine());
+			int z = x / y;
+			System.out.println("z = " + z);
+		} catch (RuntimeException re) {
+			System.out.println("Exception thrown: " + re);
+		} 
+		/*
+		 * catch (ArithmeticException ae) { System.out.println("Exception thrown: " +
+		 * ae); } catch (NumberFormatException nfe) {
+		 * System.out.println("Exception thrown: " + nfe); }
+		 */
+	}
+}
