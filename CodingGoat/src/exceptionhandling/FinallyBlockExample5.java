bdeb5b911835600eb4ecb1e46eacae23eeda84ee
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:29:04 +0530

    Added FinallyBlockExample5.java

---
+++ CodingGoat/src/exceptionhandling/FinallyBlockExample5.java
@@ -0,0 +1,21 @@
+package exceptionhandling;
+
+public class FinallyBlockExample5 {
+
+	public static void main(String[] args) {
+
+		try {
+			System.out.println("111");
+			System.out.println(20 / 0);
+			System.out.println("222");
+		} catch (Exception e) {
+			System.out.println(10 / 0);
+		}
+
+		finally {
+			System.out.println("444");
+		}
+
+		System.out.println("555");
+	}
+}
