ae94f897945b2ddf05e17057503177485810f329
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:32:39 +0530

    Added Test.java

---
+++ CodingGoat/src/stringmanipulation/Test.java
@@ -0,0 +1,13 @@
+package stringmanipulation;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String str1 = "Hello";
+		String str2 = "hello";
+		System.out.println(str1 == str2);
+
+	}
+
+}
