ccc7bd4497c01d3872f3d904fc43b1b8dff5bb64
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:26:21 +0530

    Added ExceptionHandlingwithTryCatch.java

---
+++ CodingGoat/src/exceptionhandling/ExceptionDivideByZero.java
@@ -0,0 +1,14 @@
+package exceptionhandling;
+
+public class ExceptionDivideByZero {
+
+	public static void main(String[] args) {
+
+		System.out.println("One");
+		System.out.println("Two");
+
+		int a = 1 / 0;
+		System.out.println("Three");
+		System.out.println("Four");
+	}
+}
