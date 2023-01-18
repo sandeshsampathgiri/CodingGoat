c58e2302f01f1154191d13d906fd3238dd01886d
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:28:35 +0530

    Added FinallyBlockExample3.java

---
+++ CodingGoat/src/exceptionhandling/FinallyBlockExample3.java
@@ -0,0 +1,21 @@
+package exceptionhandling;
+
+public class FinallyBlockExample3 {
+
+	public static void main(String[] args) {
+
+		int a = 20, b = 0;
+		try {
+			System.out.println("Value of a:" + a);
+			System.out.println("Value of b:" + b);
+			int div = a / b;
+			System.out.println("Division=" + div);
+		} catch (NullPointerException e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("Denominator cannot be zero");
+
+		}
+		System.out.println("Hello Java");
+	}
+}
