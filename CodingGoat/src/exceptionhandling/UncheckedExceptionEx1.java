aa211ca22d1121355b76cdbcf97afbd51f57faab
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:37:30 +0530

    Added UncheckedExceptionEx1.java

---
+++ CodingGoat/src/exceptionhandling/UncheckedExceptionEx1.java
@@ -0,0 +1,12 @@
+package exceptionhandling;
+
+public class UncheckedExceptionEx1 {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 0;
+		int z = x / y;
+		System.out.println(z);
+	}
+}
