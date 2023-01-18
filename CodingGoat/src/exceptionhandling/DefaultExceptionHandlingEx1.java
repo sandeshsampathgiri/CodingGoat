426c4d2bb9f7c4f7dd553ef15bff3175852c247a
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:25:49 +0530

    Added DefaultExceptionHandlingEx1.java

---
+++ CodingGoat/src/exceptionhandling/DefaultExceptionHandlingEx1.java
@@ -0,0 +1,23 @@
+package exceptionhandling;
+
+public class DefaultExceptionHandlingEx1 {
+
+	public static void main(String[] args) {
+
+		m1();
+	}
+
+	public static void m1() {
+		m2();
+	}
+
+	public static void m2() {
+		m3();
+	}
+
+	public static void m3() {
+		System.out.println(1 / 0);
+
+	}
+
+}
