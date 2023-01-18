13fc1fc8be7de89bed5468f335bd8d0339b2b28e
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-18 17:32:59 +0530

    Added TryCatchEx1.java

---
+++ CodingGoat/src/exceptionhandling/TryCatchEx1.java
@@ -0,0 +1,14 @@
+package exceptionhandling;
+
+public class TryCatchEx1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("11");
+		System.out.println("Before divide");
+		int x = 1 / 0;
+		System.out.println("After divide");
+		System.out.println("22");
+	}
+
+}
