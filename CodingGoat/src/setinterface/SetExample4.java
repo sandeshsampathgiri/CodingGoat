6939262a0b8fbe31d59c154ab7332f9a6fb93398
sandeshsampathgiri <sandesh.sampathgiri@gmail.com>
2023-01-21 13:49:35 +0530

    Added SetExample4.java

---
+++ CodingGoat/src/setinterface/SetExample4.java
@@ -0,0 +1,29 @@
+package setinterface;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SetExample4 {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<Integer>();
+		int size = list.size();
+		System.out.println("Size before adding elements: " + size);
+		list.add(5);
+		list.add(10);
+		list.add(5);
+		list.add(15);
+		list.add(20);
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		System.out.println("Original order of List Elements");
+		System.out.println(list);
+
+		Set<Integer> s = new HashSet<Integer>(list);
+		System.out.println("Unodered List Elements after removing duplicates.");
+		System.out.println(s);
+	}
+}
